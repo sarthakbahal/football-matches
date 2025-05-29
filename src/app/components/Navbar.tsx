3de15c42023d847ad22e976a2caae85967a8f389
sarthakbahal <sarthakbahal.45@gmail.com>
2025-05-29 23:26:40 +0530

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar:FC = () => {
         <div className="flex justify-between items-center py-2 px-4 bg-gradient-to-r from-[#7f00ff] via-[#3a0073] to-[#0f051d] rounded-xl border-b-2 border-[#a259ff] shadow-lg mb-4">
             <Link href="/" className="flex items-center space-x-2">
                 <div className="relative w-[30px] h-[30px]">
-                    <Image src="/football-info.png" alt="logo" fill className="object-cover"/>
+                    <Image src="/img/football-info.png" alt="logo" fill className="object-cover"/>
                 </div>
                 <span className="text-2xl font-extrabold text-[#e0aaff] drop-shadow-[0_2px_4px_#a259ff] tracking-wide">Football Match Info</span>
             </Link>
