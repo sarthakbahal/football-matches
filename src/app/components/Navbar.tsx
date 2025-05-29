b4252dcc297ee2593fa94796c0767818f9b9b284
sarthakbahal <sarthakbahal.45@gmail.com>
2025-05-29 11:56:30 +0530

    Initial build

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+const Navbar = () => {
+    return(
+        <div className="flex justify-between items-center py-2">
+            <Link href="/" className="flex items-center space-x-2">
+                <div className="relative w-[30px] h-[30px]">
+                    <Image src="/football-info.png" alt="logo" fill className="object-cover"/>
+                </div>
+                <span className="text-2xl font-bold none md:block">Football Match Info</span>
+            </Link>
+
+            <p>Created by <Link href="https://github.com/sarthakbahal" className="text-blue-500">Sarthak Bahal</Link></p>
+        </div>
+    )
+}
+
+export default Navbar
